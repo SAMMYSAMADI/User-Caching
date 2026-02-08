e0a961cff4e35f5840d42103d39bfef4c640a8d1
SAMADIPERERA <samadiperera2011@gmail.com>
2026-02-08 09:30:50 +0530

    Add Validations for User and UpdateUserDto

--- UserCachingApi/UserCachingApi/DTOs/UpdateUserDto.cs
+++ UserCachingApi/UserCachingApi/DTOs/UpdateUserDto.cs
@@ -1,9 +1,15 @@
-﻿namespace UserCachingApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserCachingApi.DTOs
 {
     public class UpdateUserDto
     {
+        [Required]
         public string Name { get; set; } = "";
+        [Required]
         public string Username { get; set; } = "";
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = "";
     }
 }
--- UserCachingApi/UserCachingApi/Models/User.cs
+++ UserCachingApi/UserCachingApi/Models/User.cs
@@ -1,10 +1,17 @@
-﻿namespace UserCachingApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserCachingApi.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = "";
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; } = "";
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; } = "";
     }
 }
