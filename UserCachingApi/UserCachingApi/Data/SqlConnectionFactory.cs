4dffd9c827f61e3a1933eece854c168f651a0d50
SAMADIPERERA <samadiperera2011@gmail.com>
2026-02-08 06:29:05 +0530

    Add SQL connection factory

---
+++ UserCachingApi/UserCachingApi/Data/SqlConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace UserCachingApi.Data
+{
+    public class SqlConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(
+                _configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
